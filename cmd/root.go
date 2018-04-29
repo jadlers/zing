6214e3b1b14fb007456b241415374cac3a3bdf66
Jacob Adlers <jacob.adlers@gmail.com>
2018-04-29 14:22:28 +0200

    Add help for base command

--- cmd/root.go
+++ cmd/root.go
@@ -31,16 +31,15 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "zzng",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Get information about songs & artists",
+	Long: `CLI to get information on those songs and artists that
+might be playing around you at this exact moment. Or maybe
+you just have to know more about that song you've got stuck
+in your head.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Want to search for: %s\n", strings.Join(args, " "))
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
