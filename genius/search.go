c88fe11cbde0a43c4297b375d5a743ed8eb2d3a7
Jacob Adlers <jacob.adlers@gmail.com>
2018-04-29 15:06:59 +0200

    Make GetLinksFor return name of first result

--- genius/search.go
+++ genius/search.go
@@ -15,6 +15,8 @@ import (
 
 var clientAccessToken string
 
+const baseURL = "https://api.genius.com/"
+
 func init() {
 	err := godotenv.Load()
 	if err != nil {
@@ -26,17 +28,29 @@ func init() {
 // GetLinksFor searches the genius database for song & artist info for a query.
 // It returns a string including links to more info on the best search result.
 func GetLinksFor(query string) string {
-	return "Will search for: " + query
+	searchRes := makeSearchRequest(query)
+	linkString := extractLinks(searchRes)
+	return linkString
 }
 
-const baseURL = "https://api.genius.com/"
+func extractLinks(res SearchResponse) string {
+	if len(res.Response.Hits) < 1 {
+		return "No result"
+	}
+	fstHit := res.Response.Hits[0].Result
+	fullTitle := fstHit.FullTitle
+	links := fmt.Sprintf(`Showing links for: %s`, fullTitle)
+	return links
+}
 
 func urlSafeString(s string) string {
 	spacesRemoved := strings.Replace(s, " ", "%20", -1)
 	return spacesRemoved
 }
 
-func makeRequest(url string) SearchResponse {
+func makeSearchRequest(search string) SearchResponse {
+	safeString := urlSafeString(search)
+	url := searchRequestURL(safeString)
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
